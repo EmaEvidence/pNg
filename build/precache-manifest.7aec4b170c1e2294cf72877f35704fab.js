b2eb6a7bf735832dc8df7a40cc3a496783408e8d
EmaEvidence <emmanuelalabi563@gmail.com>
2018-11-17 14:18:08 +0100

    configure for production build

---
+++ build/precache-manifest.7aec4b170c1e2294cf72877f35704fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b938a163e78e83ee24c969fb9dab44ee",
+    "url": "/static/media/FusiCondensedNormal.b938a163.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81fd7bdbbf49d5f2ec0e",
+    "url": "/static/js/main.81fd7bdb.chunk.js"
+  },
+  {
+    "revision": "598e747a796f4c0e6d86",
+    "url": "/static/js/1.598e747a.chunk.js"
+  },
+  {
+    "revision": "81fd7bdbbf49d5f2ec0e",
+    "url": "/static/css/main.27bcb840.chunk.css"
+  },
+  {
+    "revision": "61be13649ca1eb11589889e59eaa348c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import Route from './routes/index';
+import Route from './server/routes/index';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/static', express.static(path.join(path.dirname(__dirname), 'build/static')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -19,7 +27,7 @@ const port = process.env.port || 9000;
 
 app.use(Route)
 app.listen(port, () => {
-  console.log('We are live here');
+  console.log(`We are live here${port}`);
 });
 
 export default app;
